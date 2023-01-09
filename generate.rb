25ab4f1dd72a12b00409b09c3eb6faf5cf5816eb
Shaumik-Ashraf <shaumikashraf@mitre.org>
2023-01-09 16:30:43 -0500

    debugging generator

--- generate.rb
+++ generate.rb
@@ -21,19 +21,35 @@ class Generate < Thor
     end
 
 
-    desc "chapter TITLE [--filename NAME] [--permalink LINK]" "Add a new chapter in playbook collection"
-    method_option :filename, :type => :string, :aliases => '-f'
+    desc "chapter TITLE [--filename NAME] [--permalink LINK]", "Add a new chapter in playbook collection"
+    method_option :dirname, :type => :string, :aliases => '-d'
     method_option :permalink, :type => :string, :aliases => '-p'
     def chapter(title)
         @title = title
-        @filename = options[:filename] || title.downcase.gsub(' ', '_').underscore
-        @permalink = options[:permalink] || @filename.dasherize
-        path = File.join(PLAYBOOK_PATH, @filename += '.md')
+        @dirname = options[:dirname] || title.downcase.gsub(' ', '_').underscore
+        @permalink = options[:permalink] || @dirname.dasherize
+        dirpath = File.join(PLAYBOOK_PATH, @dirname, 'index.md')
 
-        FileUtils.mkdir_p( path )
-        template('index.md.erb', path)
+        FileUtils.mkdir_p( dirpath )
+        template('index.md.erb', File.join(dirpath, 'index.md'))
     end
 
+    desc "page TITLE --dirname DIR [--filename NAME] [--permalink LINK] [--chapter PARENT] [--order N]", "Add a new page in a playbook chapter"
+    method_option :filename, :type => :string, :aliases => '-f'
+    method_option :permalink, :type => :string, :aliases => '-p'
+    method_option :chapter, :type => :string, :aliases => '-c'
+    method_option :dirname, :type => :string, :aliases => '-d', :required => true
+    method_option :order, :type => :numeric, :aliases => '-o'
+    def page(title)
+        @title = title
+        @filename = options[:filename] || title.downcase.gsub(' ', '_').underscore
+        @chapter = options[:chapter] || options[:dirname].titleize
+        @permalink = options[:permalink] || File.join( options[:dirname], @filename.dasherize )
+        @order = options[:order] || count_pages(File.join(PLAYBOOK_PATH, options[:dirname]))
+        path = File.join(PLAYBOOK_PATH, options[:dirname], @filename += '.md')
+
+        template('page.md.erb', path)
+    end
 
     desc "section_link CHAPTER SECTION", "Add markdown section link to nav child links, use pretty args"
     method_option :order, :type => :numeric, :aliases => '-o'
@@ -47,7 +63,7 @@ class Generate < Thor
         @permalink = "/#{parent.dasherize}##{section.dasherize}"
 
         if options[:order]
-            @prder = options[:order].to_i
+            @order = options[:order].to_i
         else # count files in _playbook/<parent>/ to derive link order number
             parent_dirname = Dir.children(PLAYBOOK_PATH).select { |subdir| subdir.upcase.match? parent.upcase }.first
             @order = Dir.children( File.join(PLAYBOOK_PATH, parent_dirname) ).reject { |f| f.starts_with? '.' }.length
@@ -58,6 +74,14 @@ class Generate < Thor
         file_path = File.join(PLAYBOOK_PATH, parent_dirname, "#{order_str}_#{section}.md")
         
         template("section_link.md.erb", file_path);
+    end
+
+
+    private
+
+    # count number of markdown files in directory
+    def count_pages(dir_path)
+        Dir.children(dir_path).select { |f| f.ends_with? '.md' }.length
     end
 
 end
