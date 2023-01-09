600f8bedb6f51de0512b43ed2f092f2ecb24a1bc
Shaumik-Ashraf <shaumikashraf@mitre.org>
2023-01-09 12:21:08 -0500

    debug generator

--- generate.rb
+++ generate.rb
@@ -6,7 +6,7 @@
 #
 
 require 'thor'
-require 'active_support'
+require 'active_support/core_ext/string'
 
 class Generate < Thor
 
@@ -20,19 +20,27 @@ class Generate < Thor
     end
 
 
-    desc "generate:section_link CHAPTER SECTION", "Add markdown section link to nav child links"
+    desc "section_link CHAPTER SECTION", "Add markdown section link to nav child links, use pretty args"
+    method_option :order, :type => :numeric, :aliases => '-o'
     def section_link(parent, section)
         @parent = parent
         @title = section
-        @permalink = "#{parent.underscore}##{section.dasherize}"
 
-        # count files in _playbook/<parent>/ to derive link order number
-        parent_dirname = Dir.children(PLAYBOOK_PATH).select { |subdir| subdir.upcase.match? parent.underscore.upcase }.first
-        say("Error: path not found for #{parent}", :red) and return if parent_dirname.nil?
-        order = Dir.children( File.join(PLAYBOOK_PATH, parent_dirname) ).length
-        @order = (order > 9) ? order.to_s : "0#{order}"
+        parent = parent.gsub(' ', '_').downcase.underscore
+        section = section.gsub(' ', '_').downcase.underscore
 
-        file_path = File.join(PLAYBOOK_PATH, parent_dirname, "#{@order}_#{section.underscore}.md")
+        @permalink = "/#{parent}##{section.dasherize}"
+
+        if options[:order]
+            @prder = options[:order].to_i
+        else # count files in _playbook/<parent>/ to derive link order number
+            parent_dirname = Dir.children(PLAYBOOK_PATH).select { |subdir| subdir.upcase.match? parent.upcase }.first
+            @order = Dir.children( File.join(PLAYBOOK_PATH, parent_dirname) ).reject { |f| f.starts_with? '.' }.length
+            say("Identified directory #{parent_dirname} for #{@parent}, using order #{@order}", :yellow)
+        end
+        order_str = (@order > 9) ? @order.to_s : "0#{@order}"
+
+        file_path = File.join(PLAYBOOK_PATH, parent_dirname, "#{order_str}_#{section}.md")
         
         template("section_link.md.erb", file_path);
     end
